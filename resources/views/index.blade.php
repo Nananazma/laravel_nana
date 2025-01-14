1955cd35e1f7d0992d05ba7b4fe70a145bfdec85
Nazma nurhaliza <nurhalizanazma@gmail.com>
2025-01-14 09:53:12 +0700

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Media</title>
+</head>
+<body>
+    <h1>Media Online</h1>
+    <h3>Sosial Media Developer</h3>
+    <p>belajar dan berbagi agar hidup lebih baik</p>
+    <h4>Benefit join di Media online</h4>
+
+    <ul>
+        <li>Mendapatkan motivasi dari sesama para Developer </li>
+        <LI>Sharing Klowlenge</LI>
+        <li>Dibuat oleh calon web developer terbaik</li>
+        </ul>
+
+        <h3>Cara bergabung ke Media Online</h3>
+
+        <ol>
+            <li>Mengunjungi website ini</li>
+            <li>Mendaftarkan di <a href="/form">Form sign Up</a></li>
+            <LI>Selesai</LI>
+            </ol>
+</body>
+</html>
