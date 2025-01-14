d070acd51775e0fec52798530038412a909e1dee
Nazma nurhaliza <nurhalizanazma@gmail.com>
2025-01-14 10:04:23 +0700

    laravel

---
+++ resources - Copy/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Selamat datang</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG!</h1>
+    <h3>Terima kasih telah bergabung di Website kami. Media belajar kita bersama!</h3>
+   <img src="{{ asset('img/download.jpg') }}" alt="download"
+    width="200px" width="300" height="200px">
+
+<video controls="320" width="300" height="240" autoplay>
+    <source src={{ asset("vid/vidio.mp4")}} type="video/mp4">
+</video>
+</body>
+</html>
